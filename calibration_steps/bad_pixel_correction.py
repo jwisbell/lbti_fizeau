fixed rotation angle error, nan error with rotation, and cleaned up some defaults

calibration_steps/bad_pixel_correction.py
@@ -71,7 +71,7 @@ def bpm_test():
     return 1
 
 
-def correct_image_after_bpm(masked_image):
+def correct_image_after_bpm(masked_image, skip=False):
     # find each bad pixel and replace with the median of its neighbors
     # what do we do about the edges? (skip?)
 
@@ -80,6 +80,8 @@ def correct_image_after_bpm(masked_image):
     #     for j in range(len(masked_image[i])):
     #         if masked_image[i, j] == 0:
     #             zero_locations.append((i, j))
+    if skip:
+        return masked_image
 
     zero_locations = np.where(masked_image == 0)
 
@@ -111,13 +113,14 @@ def correct_image_after_bpm(masked_image):
     return corrected_image
 
 
-def apply_bad_pixel_mask(bpm, bkg_sub_ims):
+def apply_bad_pixel_mask(bpm, bkg_sub_ims, skip=False):
     # actually apply the bad pixel mask
     # input npm is a binary mask (True means good pixel, False means bad pixel)
     # ensure that all bad pixels are marked as np.nan for easier processing later
+    if skip:
+        return bkg_sub_ims
 
     masked_ims = [bpm * x for x in bkg_sub_ims]
-
     return masked_ims
 
 
@@ -131,16 +134,17 @@ def load_bpm(im_hdr):
     # bpm[~bpm] = np.nan
 
     # print(bpm.shape)  # should be 2048x2048
+    try:
+        # get the proper readout region with desired shape
+        x1 = int(im_hdr["SUBSECX1"]) - 1
+        x2 = int(im_hdr["SUBSECX2"]) - 1
+        y1 = int(im_hdr["SUBSECY1"]) - 1
+        y2 = int(im_hdr["SUBSECY2"]) - 1
 
-    # get the proper readout region with desired shape
-    x1 = int(im_hdr["SUBSECX1"]) - 1
-    x2 = int(im_hdr["SUBSECX2"]) - 1
-    y1 = int(im_hdr["SUBSECY1"]) - 1
-    y2 = int(im_hdr["SUBSECY2"]) - 1
-
-    bpm_window = bpm[y1 : y2 + 1, x1 : x2 + 1]
-
-    return bpm_window
+        bpm_window = bpm[y1 : y2 + 1, x1 : x2 + 1]
+        return bpm_window
+    except KeyError:
+        return bpm
 
 
 if __name__ == "__main__":

reduction_steps/do_background_subtraction.py
@@ -20,6 +20,8 @@ import time
 import polars as pl
 import pickle
 import os
+from astropy.stats import sigma_clip
+import numexpr as ne
 
 from calibration_steps.bad_pixel_correction import (
     correct_image_after_bpm,
@@ -68,6 +70,123 @@ def _merge_headers_to_df(hdr_dicts, nod_name):
     return df
 
 
+def _fast_fit_slope(imgcube):
+    # Mask NaN and Inf values
+    imgcube = np.ma.masked_invalid(imgcube)
+
+    # Create time vector (starting from 1)
+    time = np.arange(imgcube.shape[0], dtype=np.float64) + 1
+
+    # Prepare shape for broadcasting
+    tshape = tuple(np.roll(imgcube.shape, -1))
+
+    # Core precomputed quantities
+    time_reshaped = np.transpose(np.resize(time, tshape), (2, 0, 1))
+    time_sq_reshaped = np.transpose(np.resize(np.square(time), tshape), (2, 0, 1))
+
+    Sx = np.ma.array(time_reshaped).sum(axis=0, dtype=np.float64)
+    Sxx = np.ma.array(time_sq_reshaped).sum(axis=0, dtype=np.float64)
+    Sy = np.ma.mean(imgcube, axis=0, dtype=np.float64)
+    Sxsx = Sx * Sx
+    Sxy = (imgcube * time[:, np.newaxis, np.newaxis]).sum(axis=0, dtype=np.float64)
+    n = np.ma.count(imgcube, axis=0)
+
+    # Apply regression formula (NaN-safe due to masked arrays)
+    beta = ne.evaluate(
+        "(((Sx / n) * Sy) - (Sxy / n)) / (((Sxsx / (n * n))) - (Sxx / n))"
+    )
+    alpha = ne.evaluate("Sy - (beta * (Sx / n))")
+
+    # Fill masked values (if desired)
+    beta = np.ma.filled(beta, np.nan)
+    alpha = np.ma.filled(alpha, np.nan)
+
+    return beta, alpha
+
+
+def _fix_gain(image):
+    # for each channel (spaced by XX pixels), find the median value of the top 100 and bottom 100 pixels and subtract out
+    new_image = np.copy(image)
+    channel_width = 64
+    channel_region = 50
+    for xstart in np.arange(0, image.shape[0] + channel_width, channel_width):
+        region_top = image[0:channel_region, xstart : xstart + channel_width]
+        region_bot = image[-channel_region:, xstart : xstart + channel_width]
+        # fig, (ax, bx) = plt.subplots(1, 2)
+        # ax.imshow(region_top)
+        # bx.imshow(region_bot)
+        # plt.show()
+        # plt.close()
+        channel_val = np.mean([np.median(region_top), np.median(region_bot)])
+        new_image[:, xstart : xstart + channel_width] -= channel_val
+
+    return new_image
+
+
+def _ramp_fitting(im_arr, spacing=1, do_plot=False, full_fit=False):
+    # uses the exposure ramp to suppress read noise
+    s = im_arr[0].shape[0]
+
+    # no bias subtraction
+    delta_ims = np.array([im for im in im_arr[::spacing]]).astype("float")
+
+    # sigma clip each image
+    for i in range(len(delta_ims)):
+        delta_ims[i] = sigma_clip(delta_ims[i])
+
+    std_vals = [np.nanstd(im) for im in delta_ims]
+    try:
+        std_vals[0] = std_vals[1]
+    except IndexError:
+        pass
+
+    alpha = np.zeros(im_arr[0].shape)
+    beta, alpha = _fast_fit_slope(delta_ims)
+
+    if do_plot:
+        fig, ax = plt.subplots()
+        slope1 = beta[s // 2 + 1, s // 2 + 1]
+        alpha1 = alpha[s // 2 + 1, s // 2 + 1]
+        upperleft = (512, 1550)
+        slope2 = beta[upperleft[1], upperleft[0]]
+        alpha2 = alpha[upperleft[1], upperleft[0]]
+        xvals = np.arange(len(im_arr))
+        ax.plot(xvals, slope1 * xvals + alpha1)
+        ax.errorbar(
+            xvals,
+            [di[s // 2 + 1, s // 2 + 1] for di in delta_ims],
+            yerr=std_vals,
+            ls="none",
+            marker="s",
+        )
+
+        ax.plot(xvals, slope2 * xvals + alpha2)
+        ax.errorbar(
+            xvals,
+            [di[upperleft[1], upperleft[0]] for di in delta_ims],
+            yerr=0,
+            ls="none",
+            marker="s",
+        )
+        plt.tight_layout()
+
+        fig2 = plt.figure()
+        plt.imshow(
+            beta * len(im_arr) + alpha * 0,
+            origin="lower",
+            vmin=0,
+            vmax=600,
+            cmap="Spectral_r",
+        )
+        # plt.savefig("./example_ramps.png")
+        plt.show()
+        # plt.close()
+
+    # additionally subtract out the channel biases (due to gain)
+    corrected_im = _fix_gain(beta * len(im_arr) + alpha * 0)
+    return corrected_im
+
+
 def _savefits(bkg_subbed_images, nod_key, headers, config, process_path):
     start_im = config["nod_info"][nod_key]["start"]
     end_im = config["nod_info"][nod_key]["end"]
@@ -85,6 +204,7 @@ def _savefits(bkg_subbed_images, nod_key, headers, config, process_path):
         hdr[key] = value
     hdu = fits.PrimaryHDU(data=bkg_subbed_images, header=hdr)
     hdul = fits.HDUList([hdu])
+    print("Saving fits file")
     hdul.writeto(
         f"{output_dir}/{process_path}/{target}_bkgsub_nod{nod_key}_{band}_{obsdate}_{str(start_im).zfill(6)}-{str(end_im).zfill(6)}.fits",
         overwrite=True,
@@ -116,6 +236,7 @@ def _load_fits_files(
     prefix,
     skipkeys=[],
     ramp_params: dict = {"idx": -1, "subtract_min": False},
+    do_up_the_ramp=False,
 ):
     # for each nod position open the files
     # extract a box of size `aperture size` nod position in each file
@@ -166,6 +287,8 @@ def _load_fits_files(
                             # subtracting out the "zero" exposure to remove bad pixels
                             if ramp_params["subtract_min"]:
                                 im -= x[0].data[0]
+                            if do_up_the_ramp:
+                                im = _ramp_fitting(x[0].data)
 
                     temp.append(_extract_window(im, entry["position"]))
                     # temp.append(im)
@@ -255,6 +378,96 @@ def _qa_plots(bg_subtracted_frames, centroid_positions, timestamps, output_dir, 
     plt.close("all")
 
 
+def _parse_config(config):
+    # extract relevant info from config file
+    target = config["target"]
+    nod_info = config["nod_info"]
+    instrument = config["instrument"]
+    extraction_size = config["sub_window"]  # 100
+    data_dir = config["data_dir"]
+    obsdate = config["obsdate"]
+    output_dir = config["output_dir"]
+    skips = [str(x) for x in config["skips"]]
+    batch_size = config["batch_size"]
+    try:
+        do_up_the_ramp = config["do_up_the_ramp"]
+    except KeyError:
+        do_up_the_ramp = False
+
+    try:
+        save_fits = config["save_fits"]
+    except KeyError:
+        save_fits = False
+
+    try:
+        ramp_params = config["ramp_params"]
+    except KeyError:
+        ramp_params = {"idx": -1, "subtract_min": True}
+        logger.warn(
+            PROCESS_NAME,
+            f"No ramp params set, using default {ramp_params}",
+        )
+
+    try:
+        skip_bpm = config["skip_bpm_correction"]
+    except KeyError:
+        skip_bpm = False  # do bad pixel correction by default
+
+    return (
+        target,
+        nod_info,
+        instrument,
+        extraction_size,
+        data_dir,
+        obsdate,
+        output_dir,
+        skips,
+        batch_size,
+        do_up_the_ramp,
+        save_fits,
+        ramp_params,
+        skip_bpm,
+    )
+
+
+def _get_filenames(data_dir, prefix, start_fn, end_fn, sub_start_fn, sub_end_fn):
+    obj_files = [
+        f"{data_dir}/{prefix}{str(i).zfill(6)}.fits"
+        for i in range(start_fn, end_fn + 1)
+    ]
+    bkg_files = [
+        f"{data_dir}/{prefix}{str(i).zfill(6)}.fits"
+        for i in range(sub_start_fn, sub_end_fn + 1)
+    ]
+
+    # Check if we are dealing with fits or fits.gz
+    test_fname = obj_files[np.random.randint(0, len(obj_files))]
+    if os.path.exists(test_fname):
+        print("Using uncompressed target files")
+    else:
+        print(
+            "Using compressed target files (*fits.gz) -- NOTE: this takes longer to process"
+        )
+        obj_files = [
+            f"{data_dir}/{prefix}{str(i).zfill(6)}.fits.gz"
+            for i in range(start_fn, end_fn + 1)
+        ]
+
+    test_fname = bkg_files[np.random.randint(0, len(bkg_files))]
+    if os.path.exists(test_fname):
+        print("Using uncompressed background files")
+    else:
+        print(
+            "Using compressed background files (*fits.gz) -- NOTE: this takes longer to process"
+        )
+        bkg_files = [
+            f"{data_dir}/{prefix}{str(i).zfill(6)}.fits.gz"
+            for i in range(sub_start_fn, sub_end_fn + 1)
+        ]
+
+    return obj_files, bkg_files
+
+
 def _old_bkg_subtraction(
     nod_info,
     data_dir,
@@ -265,6 +478,10 @@ def _old_bkg_subtraction(
     output_dir,
     process_path,
     target,
+    save_fits=False,
+    config=None,
+    do_up_the_ramp=False,
+    skip_bpm=False,
 ):
     list_keys = np.array(list(nod_info.keys()))
     num_entries = len(nod_info.keys())
@@ -282,7 +499,12 @@ def _old_bkg_subtraction(
         )
 
         ims, rotations, _, timestamps, hdr_dicts = _load_fits_files(
-            data_dir, nod_info, prefix, skipkeys=temp_skips, ramp_params=ramp_params
+            data_dir,
+            nod_info,
+            prefix,
+            skipkeys=temp_skips,
+            ramp_params=ramp_params,
+            do_up_the_ramp=do_up_the_ramp,
         )
 
         # ## Do background subtraction and extract in a window
@@ -304,27 +526,19 @@ def _old_bkg_subtraction(
             ]
             # 2.5.b. multiply the images by the bpm
             bpm_windowed = _extract_window(bpm, nod_info[key]["position"])
-            masked_images = apply_bad_pixel_mask(bpm_windowed, bkg_subbed)
+            masked_images = apply_bad_pixel_mask(
+                bpm_windowed, bkg_subbed, skip=skip_bpm
+            )
 
-            # # 3. Crop each frame to the right size
-            # cropped_ims = [
-            #     _extract_window(im, nod_info[key]["position"]) for im in masked_images
-            # ]
-
-            # 3a Correct the bad pixels with the median of the neighbors
-            corrected_ims = [correct_image_after_bpm(im) for im in masked_images]
-
+            # 3 Correct the bad pixels with the median of the neighbors
+            corrected_ims = [
+                correct_image_after_bpm(im, skip=skip_bpm) for im in masked_images
+            ]
             bg_subtracted_frames[key] = np.array(corrected_ims)
 
-        # crop the images to the selected window
-        # bg_subtracted_frames = {
-        #     key: _window_background_subtraction(
-        #         ims[key],
-        #         backgrounds[nod_info[key]["subtract"]]["mean"],
-        #         nod_info[key]["position"],
-        #     )
-        #     for key in ims.keys()
-        # }
+            # optionally save as fits files
+            if save_fits:
+                _savefits(corrected_ims, key, hdr_dicts[key], config, process_path)
 
         # save the background-subtracted sub-windows in processed data folder
         centroid_positions = {}
@@ -335,10 +549,10 @@ def _old_bkg_subtraction(
             # if "bkg" in key or "off" in key:
             # continue
             im = np.sum(bg_subtracted_frames[key], 0)
-            im = median_filter(im, 7)
+            im = median_filter(im, 5)
             centroid_positions[key] = [
-                np.argmax(np.nansum(im, 0)),
-                np.argmax(np.nansum(im, 1)),
+                np.clip(np.argmax(np.nansum(im, 0)), 32, len(im) - 32),
+                np.clip(np.argmax(np.nansum(im, 1)), 32, len(im) - 32),
             ]
             print(centroid_positions)
             # if extraction_size >= ims[key][0].shape[0]:
@@ -405,29 +619,21 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
 
     logger = mylogger
 
-    # extract relevant info from config file
-    target = config["target"]
-    nod_info = config["nod_info"]
-    instrument = config["instrument"]
-    extraction_size = config["sub_window"]  # 100
-    data_dir = config["data_dir"]
-    obsdate = config["obsdate"]
-    output_dir = config["output_dir"]
-    skips = [str(x) for x in config["skips"]]
-    batch_size = config["batch_size"]
-    try:
-        save_fits = config["save_fits"]
-    except KeyError:
-        save_fits = False
-
-    try:
-        ramp_params = config["ramp_params"]
-    except KeyError:
-        logger.warn(
-            PROCESS_NAME,
-            "No ramp params set, using default {idx:-1, subtract_min:False}",
-        )
-        ramp_params = {"idx": -1, "subtract_min": False}
+    (
+        target,
+        nod_info,
+        instrument,
+        extraction_size,
+        data_dir,
+        obsdate,
+        output_dir,
+        skips,
+        batch_size,
+        do_up_the_ramp,
+        save_fits,
+        ramp_params,
+        skip_bpm,
+    ) = _parse_config(config)
 
     prefix = f"n_{obsdate}_"
     if instrument != "NOMIC":
@@ -436,7 +642,12 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
     process_path = f"intermediate/{PROCESS_NAME}/"
 
     try:
-        from fits_lizard import subtract_mean_from_listX
+        from fits_lizard import subtract_mean_from_list
+
+        if do_up_the_ramp:
+            # force the script into python mode since up the ramp fitting
+            # not yet working in the rust package
+            raise ModuleNotFoundError
 
         bg_subtracted_frames = {}
         rotations = {}
@@ -454,45 +665,15 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
 
             sub_start_fn = nod_info[sub_key]["start"]
             sub_end_fn = nod_info[sub_key]["end"]
-
-            obj_files = [
-                f"{data_dir}/{prefix}{str(i).zfill(6)}.fits"
-                for i in range(start_fn, end_fn + 1)
-            ]
-            bkg_files = [
-                f"{data_dir}/{prefix}{str(i).zfill(6)}.fits"
-                for i in range(sub_start_fn, sub_end_fn + 1)
-            ]
-
-            # Check if we are dealing with fits or fits.gz
-            test_fname = obj_files[np.random.randint(0, len(obj_files))]
-            if os.path.exists(test_fname):
-                print("Using uncompressed target files")
-            else:
-                print(
-                    "Using compressed target files (*fits.gz) -- NOTE: this takes longer to process"
-                )
-                obj_files = [
-                    f"{data_dir}/{prefix}{str(i).zfill(6)}.fits.gz"
-                    for i in range(start_fn, end_fn + 1)
-                ]
-
-            test_fname = bkg_files[np.random.randint(0, len(bkg_files))]
-            if os.path.exists(test_fname):
-                print("Using uncompressed background files")
-            else:
-                print(
-                    "Using compressed background files (*fits.gz) -- NOTE: this takes longer to process"
-                )
-                bkg_files = [
-                    f"{data_dir}/{prefix}{str(i).zfill(6)}.fits.gz"
-                    for i in range(sub_start_fn, sub_end_fn + 1)
-                ]
+            # 1. Get the filenames to load
+            obj_files, bkg_files = _get_filenames(
+                data_dir, prefix, start_fn, end_fn, sub_start_fn, sub_end_fn
+            )
 
             # 2. Calculate the bg_subtracted_frames for that key
             start = time.time()
             result = subtract_mean_from_list(
-                obj_files, bkg_files
+                obj_files, bkg_files, do_up_the_ramp, ramp_params["subtract_min"]
             )  # returns images, rotations, julian dates
             bkg_sub_ims = [x[0] for x in result[0]]
             rots = [x[1] for x in result[0]]
@@ -510,12 +691,11 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
                 pickle.dump(polars_df, f)
 
             # 2.5 apply the bad pixel map
-            # multiply the BPM with each image
             # 2.5.a. load the bad pixel map
             bpm = load_bpm(hdr_dicts[0])
 
             # 2.5.b. multiply the images by the bpm
-            masked_images = apply_bad_pixel_mask(bpm, bkg_sub_ims)
+            masked_images = apply_bad_pixel_mask(bpm, bkg_sub_ims, skip=skip_bpm)
 
             # 3. Crop each frame to the right size
             cropped_ims = [
@@ -523,7 +703,9 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
             ]
 
             # 3a Correct the bad pixels with the median of the neighbors
-            corrected_ims = [correct_image_after_bpm(im) for im in cropped_ims]
+            corrected_ims = [
+                correct_image_after_bpm(im, skip=skip_bpm) for im in cropped_ims
+            ]
 
             # 4. Save the cropped frames in the bg_subtracted_frames dict
             bg_subtracted_frames[key] = np.array(corrected_ims)
@@ -543,10 +725,10 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
             # if "bkg" in key or "off" in key:
             # continue
             im = np.sum(bg_subtracted_frames[key], 0)
-            im = median_filter(im, 7)
+            im = median_filter(im, 5)
             centroid_positions[key] = [
-                np.argmax(np.nansum(im, 0)),
-                np.argmax(np.nansum(im, 1)),
+                np.clip(np.argmax(np.nansum(im, 0)), 32, len(im) - 32),
+                np.clip(np.argmax(np.nansum(im, 1)), 32, len(im) - 32),
             ]
 
             # if extraction_size >= ims[key][0].shape[0]:
@@ -599,6 +781,10 @@ def do_bkg_subtraction(config: dict, mylogger: Logger) -> bool:
             output_dir,
             process_path,
             target,
+            do_up_the_ramp=do_up_the_ramp,
+            save_fits=save_fits,
+            config=config,
+            skip_bpm=skip_bpm,
         )
 
     logger.info(PROCESS_NAME, "Background subtraction is done!")

reduction_steps/do_frame_selection.py
@@ -259,7 +259,11 @@ def _frame_centering_and_selection(
     # this is for using a small window
     if not use_full_window:
         cutout_loc = np.copy(centroid_positions[nod])
-        cutout_loc[0] = cutout_loc[0]
+        print(cutout_loc)
+        max_loc = bg_subtracted_frames[nod][0].shape[0]
+        cutout_loc[0] = np.clip(cutout_loc[0], 32, max_loc - 32)
+        cutout_loc[1] = np.clip(cutout_loc[1], 32, max_loc - 32)
+        print(cutout_loc)
         window_size = custom_window_size
 
     ###############################################################################

reduction_steps/do_image_corotation.py
@@ -14,7 +14,8 @@ import pickle
 from scipy.ndimage import rotate, median_filter
 from glob import glob
 from utils.util_logger import Logger
-from utils.utils import argmax2d
+from utils.utils import argmax2d, nanmedian_filter
+from astropy.io import fits
 
 PROCESS_NAME = "corotate"
 
@@ -80,9 +81,8 @@ def _plot_cycles(
 
     for k, key in enumerate(imdict.keys()):
         ims = imdict[key]["ims"]
-        rotim, _, _ = recenter(np.mean(ims, 0))
+        rotim, _, _ = recenter(np.nanmean(ims, 0))
         # rotim = np.mean(ims, 0)
-
         ax = axarr.flatten()[k]
 
         ax.imshow(rotim, origin="lower", norm=PowerNorm(0.5))
@@ -142,6 +142,7 @@ def _process_rotations(
         cims = background_subtracted_frames[key]
 
         rotations = all_rotations[f"{key}"]
+
         # cc_vals = info["correlation_vals"]
         mask = info["mask"]
         if use_phase:
@@ -153,6 +154,7 @@ def _process_rotations(
         temp_imarr = []
         temp_rotarr = []
         temp_unrotarr = []
+        temp_experimental = []
 
         for i, cim in enumerate(cims):
             if mask[i] == 1:
@@ -165,10 +167,22 @@ def _process_rotations(
 
                 # rotate to North
                 pa = rotations[i]
-                rotim = rotate(new_im, -pa, reshape=False, mode="nearest")
+                # have to remove nans otherwise this returns all nan image
+                rotim = rotate(
+                    np.nan_to_num(new_im), -pa, reshape=False, mode="nearest"
+                )
+
+                # EXPERIMENTAL -- keep only the highest resolution part
+                new_im_masked = np.copy(np.nan_to_num(new_im))
+                middle = new_im_masked.shape[0] // 2
+                new_im_masked[: middle - 1, :] = 0
+                new_im_masked[middle + 1 :, :] = 0
+                rotim_masked = rotate(new_im_masked, -pa, reshape=False, mode="nearest")
+
                 temp_imarr.append(rotim)
                 temp_rotarr.append(pa)
                 temp_unrotarr.append(new_im)
+                temp_experimental.append(rotim_masked)
 
                 # unrotated_ims.append(new_im)
                 # properly_rotated_ims.append(rotim)
@@ -176,18 +190,20 @@ def _process_rotations(
         temp_imarr = np.array(temp_imarr)
         temp_rotarr = np.array(temp_rotarr)
         temp_unrotarr = np.array(temp_unrotarr)
+        temp_experimental = np.array(temp_experimental)
         # temp_imarr = temp_imarr[mask]
         # temp_rotarr = temp_rotarr[mask]
 
         proper_rotations[f"{key}"]["ims"] = np.copy(temp_imarr)
         proper_rotations[f"{key}"]["rots"] = np.copy(temp_rotarr)
         proper_rotations[f"{key}"]["centered_unrot"] = np.copy(temp_unrotarr)
+        proper_rotations[f"{key}"]["experimental"] = np.copy(temp_experimental)
         del cims
     return proper_rotations
 
 
 def recenter(im):
-    x, y = argmax2d(median_filter(im, 3))
+    x, y = argmax2d(nanmedian_filter(im, 3))
 
     new_im = np.roll(im, im.shape[1] // 2 - x, axis=1)
     new_im = np.roll(new_im, im.shape[0] // 2 - y, axis=0)
@@ -278,10 +294,15 @@ def do_image_corotation(config: dict, mylogger: Logger) -> bool:
     sum_std = 0
     count = 0
     all_rotated = []
-    for _, entry in rotation_dict.items():
+    for nod, entry in rotation_dict.items():
         # for some reason these need to be recentered again
         centered_rot, _, _ = recenter(np.sum(entry["ims"], 0))
         centered_unrot, x, y = recenter(np.sum(entry["centered_unrot"], 0))
+        # EXPERIMENTAL
+        np.save(
+            f"{output_dir}/intermediate/{PROCESS_NAME}/{target}_nod{nod}_slices.npy",
+            np.array(entry["experimental"]),
+        )
 
         if remove_bad_pixels:
             centered_rot, _ = bad_pixel_correction.identify_bad_pixels(centered_rot)
@@ -400,9 +421,14 @@ def do_image_corotation_sd(config: dict, mylogger: Logger) -> bool:
         output_dir = config["output_dir"]
         skips = config["skips"]
         extraction_size = config["sub_window"]
+        obsdate = config["obsdate"]
     except KeyError as e:
         logger.error(PROCESS_NAME, f"Key missing from config: {e}")
         return False
+    try:
+        save_fits = config["save_fits"]
+    except KeyError:
+        save_fits = False
 
     # 1. Load the data
     # 1a - load the background subtracted frames
@@ -448,8 +474,8 @@ def do_image_corotation_sd(config: dict, mylogger: Logger) -> bool:
     all_rotated = []
     for _, entry in rotation_dict.items():
         # for some reason these need to be recentered again
-        centered_rot, _, _ = recenter(np.sum(entry["ims"], 0))
-        centered_unrot, x, y = recenter(np.sum(entry["centered_unrot"], 0))
+        centered_rot, _, _ = recenter(np.nansum(entry["ims"], 0))
+        centered_unrot, x, y = recenter(np.nansum(entry["centered_unrot"], 0))
         sum_rotated += centered_rot
         sum_unrotated += centered_unrot
         sum_std += np.roll(
@@ -523,4 +549,15 @@ def do_image_corotation_sd(config: dict, mylogger: Logger) -> bool:
         f"{output_dir}/intermediate/{PROCESS_NAME}/{target}_unrotated_stacked_psf.npy",
         psf_unrotated_percentiles,
     )
+
+    if save_fits:
+        hdu = fits.PrimaryHDU(data=stacked_rotated_im)
+        hdul = fits.HDUList([hdu])
+        process_path = f"intermediate/{PROCESS_NAME}/"
+        print("Saving fits file")
+        hdul.writeto(
+            f"{output_dir}/{process_path}/{target}_rotated_stacked_{obsdate}.fits",
+            overwrite=True,
+        )
+
     return True

utils/utils.py
@@ -6,7 +6,7 @@ Contains various utility functions that are re-used by multiple scripts.
 """
 
 import numpy as np
-from scipy.ndimage import median_filter
+from scipy.ndimage import median_filter, vectorized_filter
 from astropy.io import fits
 from os import mkdir, path
 
@@ -15,7 +15,15 @@ def argmax2d(arr):
     # wrapper for numpy argmax to give x,y coordinates of the max in a 2d array
     m = np.nanmax(arr)
     s = np.where(arr == m)
-    return s[1][0], s[0][0]
+    try:
+        return s[1][0], s[0][0]
+    except IndexError:
+        # return the center of the image
+        return arr.shape[0] // 2, arr.shape[1] // 2
+
+
+def nanmedian_filter(arr, kernel_size=3):
+    return vectorized_filter(arr, function=np.nanmedian, size=kernel_size)
 
 
 def imshift(im, y, x):